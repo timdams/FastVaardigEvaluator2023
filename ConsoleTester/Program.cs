504719af7642d05244f98060c94ab957e52a141b
Tim Dams <dams.tim@telenet.be>
2023-12-06 23:44:33 +0100

    Babysteps. Werkende sln/proj inlader + compilation test

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -1,10 +1,14 @@
 ﻿using FastEvalCL;
+
+using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
-using System.Diagnostics;
-using System.Text.Json;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Drawing;
+using System.Reflection;
 
 namespace ConsoleTester
 {
@@ -13,6 +17,38 @@ namespace ConsoleTester
         static void Main(string[] args)
         {
 
+            //string slnPath= @"D:\Temp\__PPUNZ\Hafid Khorta_214945_assignsubmission_file_\PP2eZitHafidKhorta\";
+            //string slnName = "PP2eZitHafidKhorta.sln";
+            //    Task<Solution> sln =FastEvalCL.SolutionHelper.LoadSolutionFromPathAsync(slnPath+slnName);
+
+            //var error = SolutionHelper.TestIfCompilesAsync(sln.Result.Projects.First());
+            //if(error.Result !=null)
+            //{
+            //    foreach (var e in error.Result)
+            //    {
+            //        var err = e.ToString().Replace(slnPath,string.Empty);
+            //        Console.WriteLine(err);
+
+            //    }
+            //}
+            string slnPath = @"D:\Temp\__TEST\";
+            var slns = SolutionHelper.LoadAllSolutionsFromPathAsync(slnPath);
+            foreach (var sl in slns.Result) 
+            {
+                var error = SolutionHelper.TestIfCompilesAsync(sl.Projects.First());
+                if (error.Result.Count() != 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("DID NOT COMPILE:"+sl.FilePath);
+
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine( "DID COMPILE:"+ sl.FilePath);
+                }
+                Console.ResetColor();
+            }
 
         }
 
--- FastEvalCL/SolutionHelper.cs
+++ FastEvalCL/SolutionHelper.cs
@@ -1,24 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace FastEvalCL
+namespace FastEvalCL;
+
+
+public class SolutionHelper
 {
-    public class SolutionHelper
+    public static List<SolutionModel> LoadAllSolutionsFromPath(string folderPath, bool tryFetchInfo = true)
     {
-        public static List<SolutionModel> LoadAllSolutionsFromPath(string folderPath, bool tryFetchInfo = true) 
+        List<SolutionModel> result = new List<SolutionModel>();
+        var allPrograms = Directory.GetFiles(folderPath, "Program.cs", SearchOption.AllDirectories);
+        foreach (var prog in allPrograms)
         {
-            List<SolutionModel> result = new List<SolutionModel>();
-            var allPrograms= Directory.GetFiles(folderPath, "Program.cs", SearchOption.AllDirectories);
-            foreach (var prog in allPrograms)
-            {
-                result.Add(new SolutionModel(prog, tryFetchInfo));
-            }
-            return result;
+            result.Add(new SolutionModel(prog, tryFetchInfo));
         }
+        return result;
+    }
 
-      
+    private static bool RegistDefaultCalled = false;
+    public static async Task<Solution> LoadSolutionFromPathAsync(string path)
+    {
+        CheckNeedRegister();
+        
+        using (var workspace = MSBuildWorkspace.Create())
+        {
+            return await workspace.OpenSolutionAsync(path);
+        }
+    }
+
+    private static void CheckNeedRegister()
+    {
+        if (!RegistDefaultCalled)
+        {
+            MSBuildLocator.RegisterDefaults();
+            RegistDefaultCalled = true;
+        }
+    }
+
+    public static async Task<List<Solution>> LoadAllSolutionsFromPathAsync(string folderPath)
+    {
+        var result = new List<Solution>();
+        var allPrograms = Directory.GetFiles(folderPath, "*.sln", SearchOption.AllDirectories);
+        foreach (var prog in allPrograms)
+        {
+            var sl = await LoadSolutionFromPathAsync(prog);
+            result.Add(sl);
+            //TODO fetch info hier?? ni echt goeie oop
+
+        }
+        return result;
+    }
+
+    public static async Task<IEnumerable<Diagnostic>> TestIfCompilesAsync(Project project)
+    {
+        var compilation = project.GetCompilationAsync().Result;
+        var errors = compilation.GetDiagnostics().Where(diagnostic => diagnostic.Severity == DiagnosticSeverity.Error);
+        return errors;
     }
 }
+
+
+
+
+
